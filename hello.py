598418e981da050f04453e972c4e6c1a8f8d4fc3
Hein Htet Zaw <107350576+tomaristark@users.noreply.github.com>
2024-01-29 16:58:37 +0630

    Initial commit

---
+++ hello.py
@@ -0,0 +1,67 @@
+#print function
+print("hello")
+
+#Data Types
+#int float String boolean 
+# int -> integer 12 321 3123 -123 -1
+# float -> 32.32 , 321.3
+#String -> "hello world"
+#boolean -> True False 1 0
+
+
+#variable  -> store data  in ram
+# overwrite 
+# = assign
+a = 12
+print(a)
+a = 43
+print(a)
+name = "Mg Kyaw kyi"
+print(name)
+checkBox = False
+print(checkBox)
+checkBox = True
+print(checkBox)
+
+
+#Operator + - * / 
+#Operation 123+ 123
+#Operand 123 3213
+
+a = 32 + 32
+print(a)
+a = 32 -12
+print(a)
+
+#+ +
+# - -
+# x *
+# /
+
+#Mathemathical  operator + - * / %
+
+print(22/7)
+print(22%7)
+
+# Assignment Operator += -= =
+d = 10
+d += 32 # d =d + 32
+d -= 32# d = d- 32
+print(d)
+
+#comparison operator  < >  ==
+
+print(32 > 10)
+print(10 > 32)
+print(10 == 10)
+
+# if (true)
+# if ( false)
+
+c= int(input("Enter a first number"))
+d =int(input("Enter second num"))
+
+operator = input("Enter The Operator")
+if(operator == '+'):
+    print(c + d)
+
